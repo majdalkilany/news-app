49a6d9633c524f4e015133cda46f6b3885ddd473
majdalkilany <majdkilany92@gmail.com>
2023-08-19 01:12:07 +0300

    update the api link

--- src/redux/newsThunk.js
+++ src/redux/newsThunk.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const fetchNews = createAsyncThunk('fetch/news', async () => {
-  const response = await axios.get(`https://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=${process.env.REACT_APP_key}`);
+  const response = await axios.get('https://news-api-v1.onrender.com/news');
   return response.data.articles;
 });
 
