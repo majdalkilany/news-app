95efdebd3061f68b452085462fccf1a52078bcd1
majdalkilany <majdkilany92@gmail.com>
2023-08-19 00:14:32 +0300

    try to solve the cors problem

--- src/redux/newsThunk.js
+++ src/redux/newsThunk.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const fetchNews = createAsyncThunk('fetch/news', async () => {
-  const response = await axios.get(`http://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=${process.env.REACT_APP_key}`);
+  const response = await axios.get(`https://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=${process.env.REACT_APP_key}`);
   return response.data.articles;
 });
 
